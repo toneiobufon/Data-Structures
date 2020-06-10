8296b0774820dc27055be513d0259f4291fa1bd7
toneiojimz <ajimeneza77@gmail.com>
2020-06-10 10:13:25 -0700

    working on BST

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -17,7 +17,10 @@ class BSTNode:
 
     # Insert the given value into the tree
     def insert(self, value):
-        pass
+        if self.value is None:
+            self.value = value
+        else:
+            if self
 
     # Return True if the tree contains the value
     # False if it does not

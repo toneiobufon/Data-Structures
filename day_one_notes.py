574527aa44157e0ddcb366ac1ec6f22847abc16d
toneiojimz <ajimeneza77@gmail.com>
2020-08-10 23:47:38 -0700

    stack done

---
+++ day_one_notes.py
@@ -0,0 +1,35 @@
+#runtime complexity is a way to objectively measure and compare
+# 
+# 
+# two runtime complexity 
+# 
+# complexity classes:
+#    constant time: efficiendy does not depend on input size
+
+#   linear time: efficiency is affected by size
+
+
+#example of runtime complexity
+
+# commands = ['n', 's', 'w', 'e']
+
+
+#constant time
+#time does not grow based on number of input
+
+# commands[3] # size of input has no bearing on the efficiency of this operation
+
+#linear time
+#linear runtime grows 1:1 as the size of input increases 
+
+# for command in commands:
+#     print(command) #input size has direct bearing on efficiency
+
+# Constant < Linear
+
+# we are comparing how quickly the efficiency grows related to input size
+
+
+#example of singly linked list below
+
+
---
+++ sample.py
@@ -0,0 +1,7 @@
+# Print out each element of the following array on a separate line:
+a = ["Joe", "2", "Ted", "4.98", "14", "Sam", "void *", "42", "float", "pointers", "5006"]
+# You may use whatever programming language you'd like.
+# Verbalize your thought process as much as possible before writing any code. 
+# Run through the UPER problem solving framework while going through your thought process.
+for i in a:
+    print(i)
